758a6c3c072421c6cfe4ee1fc04ee052e1fd01d5
Vitor Toledo <vitortoledorodovalho@gmail.com>
2020-11-04 22:20:16 -0300

    Início controle versão

---
+++ components/FormularioCadastro.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function CardNota(){
+    return(
+        <section>
+            <header>
+                <h3>Título</h3>
+            </header>
+            <p>Escreva sua nota</p>
+        </section>
+    );
+}
+export default CardNota;
